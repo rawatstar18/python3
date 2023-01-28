74c7b3e888726e9be27e063dbccd46e3e93509f8
Ubuntu <ubuntu@ip-172-31-5-180.ap-south-1.compute.internal>
2023-01-28 11:25:02 +0000

    first coding

---
+++ coding1.py
@@ -0,0 +1,11 @@
+# this program says hello and ask my name
+
+print ("hello world ")
+print ("what is your name ?") # ask for their name 
+Myname = input()
+print('its goo to meet you,'+ Myname)
+print('the lenght of your name is:')
+print(len(Myname))
+print('what is your age') #ask for their age 
+myage = input()
+print('you will be ' + str(int(myage) + 1) + ' in a year' ) 
